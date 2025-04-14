9f6040ee57ca82d32c4e7c09c69f18fa7f7d9303
I was told to put my name here, but I didn't want to <127406648+LegitKnots@users.noreply.github.com>
2025-04-14 14:00:11 -0400

    Login Fix
    fixed the stupid token validation

--- chargevault/frontend/src/components/home.jsx
+++ chargevault/frontend/src/components/home.jsx
@@ -1,35 +1,58 @@
-import React from 'react';
-import '../css/Home.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import "../css/Home.css";
+import { useNavigate } from "react-router-dom";
 import { checkToken } from "./utils/auth";
 
-
 function HomePage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    if (!checkToken()) {
-        navigate("/login");
-      }
+  useEffect(() => {
+      const runTokenCheck = async () => {
+        const isValid = await checkToken();
+        if (!isValid) {
+          navigate("/login");
+        }
+      };
+  
+      runTokenCheck();
+    }, [navigate]);
 
-    return (
-        <div className="home-container">
-            <div className="center-panel">
-                <h2>Home</h2>
-                <p>You are signed into:</p>
-                <p><strong>Midtown Atlanta Office<br />Building Renovation</strong></p>
-                <p>If this is not your construction site<br />
-                    <a href="#">click here</a></p>
+  return (
+    <div className="home-container">
+      <div className="center-panel">
+        <h2>Home</h2>
+        <p>You are signed into:</p>
+        <p>
+          <strong>
+            Midtown Atlanta Office
+            <br />
+            Building Renovation
+          </strong>
+        </p>
+        <p>
+          If this is not your construction site
+          <br />
+          <a href="#h">click here</a>
+        </p>
 
-                <div className="icon-bar">
-                    <button style={{fontSize: '32px'}} onClick={() => navigate('/home')}>🏠</button>
-                    <button style={{ fontSize: '32px' }} onClick={() => navigate('/slots')}>🔋</button>
-                    <button style={{ fontSize: '32px' }}>🔒</button>
-
-                </div>
-            </div>
-
+        <div className="icon-bar">
+          <button
+            style={{ fontSize: "32px" }}
+            onClick={() => navigate("/home")}
+          >
+            🏠
+          </button>
+          <button
+            style={{ fontSize: "32px" }}
+            onClick={() => navigate("/slots")}
+          >
+            🔋
+          </button>
+          <button style={{ fontSize: "32px" }}>🔒</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
